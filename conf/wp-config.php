0237657d69ab234340472a86239ddd19e4beb2f6
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-03-16 19:25:02 -0300

    Create wp-config.php

---
+++ conf/wp-config.php
@@ -0,0 +1,16 @@
+/** Nome da base de dados do WordPress*/
+define('DB_NAME', 'wordpress');
+/** Nome do usuário da Base de Dados do MySQL para WordPress*/
+define('DB_USER', 'wordpress');
+/** Senha do usuário da Base de Dados do MySQL para WordPress*/
+define('DB_PASSWORD', 'wordpress');
+/** Nome do Servidor do MySQL que é o Localhost*/
+define('DB_HOST', 'localhost');
+/** Configuração dos caracteres da Base de Dados, deixar o padrão*/
+define('DB_CHARSET', 'utf8');
+/** Configuração do Collate, deixar o padrão*/
+define('DB_COLLATE', '');
+/** Prefixo padrão das tabelas do Wordpress*/
+$table_prefix  = 'wp_';
+/** Configuração do Debug do Wordpress, deixar desativado (padrão false)*/
+define('WP_DEBUG', false);
