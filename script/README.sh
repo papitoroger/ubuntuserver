31cfd250eaccc942ceb530df1541b885ce20a6dc
Robson Vaamonde <robsonvaamonde@gmail.com>
2017-07-06 18:57:20 -0300

    Update README.sh

--- script/README.sh
+++ script/README.sh
@@ -1,5 +1,15 @@
-`#!/bin/bash
+#!/bin/bash
 
 # Validando o ambiente, verificando se o usuário e "root"
 USUARIO=`id -u`
 
+if [ "$USUARIO" == "0" ]
+then
+  echo -e "Usuário é `whoami`, continuando a executar o script"
+else
+  echo -e "Usuário não é ROOT, `whoami`"
+  echo -e "Pressione <Enter> para finalizar o script"
+  read
+  sleep 3
+  clear
+fi
