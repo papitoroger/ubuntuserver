040130bccaf528907ae4c842cdca19ca30604fb3
Robson Vaamonde <robsonvaamonde@gmail.com>
2018-03-12 21:01:21 -0300

    Update webmin.sh

--- aus/webmin.sh
+++ aus/webmin.sh
@@ -2,6 +2,6 @@ sudo apt-get update
 
 sudo apt-get -y install perl libnet-ssleay-perl openssl libauthen-pam-perl libpam-runtime libio-pty-perl apt-show-versions python
 
-sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.860_all.deb
+sudo wget http://prdownloads.sourceforge.net/webadmin/webmin_1.880_all.deb
 
 sudo dpkg -i webmin_1.860_all.deb
